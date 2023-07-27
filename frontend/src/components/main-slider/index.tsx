f63433e907e5511e03632f96e0a16d359c16a26c
ASGladchenko <asgladchenko@gmail.com>
2023-07-27 13:36:14 +0300

    fixed style code

--- frontend/src/components/main-slider/index.tsx
+++ frontend/src/components/main-slider/index.tsx
@@ -6,34 +6,38 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
-
 import * as Images from '../../assets/main-swiper/index';
 
-export const SliderMain = () => (
-  <div className="relative w-full h-full">
-    <h1 className="absolute block text-white top-1/2 left-1/2 z-[90] text-[45px] sm:text-[70px]  lg:text-[90px] leading-[100%] font-jost font-bold opacity-50 text-center -translate-y-full lg:-translate-y-1/2 -translate-x-1/2 lg:max-w-[600px] select-none">
-      FOUR SEASON'S TRADING
-    </h1>
-    <Swiper
-      slidesPerView={1}
-      loop={true}
-      effect="fade"
-      speed={2000}
-      allowTouchMove={false}
-      autoplay={{
-        delay: 3500,
-        disableOnInteraction: false,
-      }}
-      modules={[EffectFade, Autoplay]}
-    >
-      {Object.keys(Images).map((item) => (
-        <SwiperSlide>
-          <div
-            className={`w-full h-screen bg-cover bg-no-repeat`}
-            style={{ backgroundImage: `url(${(Images as any)[item].src})` }}
-          ></div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
-);
+import { getStyles } from './style';
+
+export const SliderMain = () => {
+  const { wrapper, title, card } = getStyles();
+
+  return (
+    <div className={wrapper}>
+      <h1 className={title}>FOUR SEASON'S TRADING</h1>
+
+      <Swiper
+        loop={true}
+        speed={2000}
+        effect="fade"
+        slidesPerView={1}
+        allowTouchMove={false}
+        modules={[EffectFade, Autoplay]}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+      >
+        {Object.keys(Images).map((item) => (
+          <SwiperSlide>
+            <div
+              className={card}
+              style={{ backgroundImage: `url(${(Images as any)[item].src})` }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
---
+++ frontend/src/components/main-slider/style.ts
@@ -0,0 +1,8 @@
+export const getStyles = () => ({
+  title:
+    'absolute block text-white top-1/2 left-1/2 z-[90] text-[45px] sm:text-[70px]  lg:text-[90px] leading-[100%] font-jost font-bold opacity-50 text-center -translate-y-full lg:-translate-y-1/2 -translate-x-1/2 lg:max-w-[600px] select-none',
+
+  wrapper: 'relative w-full h-full',
+
+  card: `w-full h-screen bg-cover bg-no-repeat`,
+});
