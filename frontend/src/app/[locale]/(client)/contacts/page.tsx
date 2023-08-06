22114855c20de2c83ef54e1ea8bb7b0c778694f8
ASGladchenko <asgladchenko@gmail.com>
2023-08-06 13:49:26 +0300

    fixed styles and create contacts

--- frontend/src/app/[locale]/(client)/contacts/page.tsx
+++ frontend/src/app/[locale]/(client)/contacts/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 style={{ height: '800px' }}>Main Contacts</h1>
-    </div>
-  );
-}
+import { Contacts } from './contacs';
+
+const ContactsPage = () => {
+  return <Contacts />;
+};
+
+export default ContactsPage;
